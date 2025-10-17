6cced92f096f8b1755733291cf8a79f50816b3ff
July <julyla123la@gmail.com>
2025-10-16 19:05:26 -0500

    ultii

--- src/worstFit.cpp
+++ src/worstFit.cpp
@@ -4,12 +4,10 @@ int WorstFit::seleccionarHueco(const vector<BloqueMemoria>& memoria, int tamano)
     int peorIdx = -1;
     int peorTam = -1;
     for (int i = 0; i < memoria.size(); ++i) {
-        if (memoria[i].estaLibre()) {
-            int tam = memoria[i].getTamano();
-            if (tam >= tamano && tam > peorTam) {
-                peorTam = tam;
-                peorIdx = i;
-            }
+        int tam = memoria[i].getTamano();
+        if (tam >= tamano && tam > peorTam) {
+            peorTam = tam;
+            peorIdx = i;
         }
     }
     return peorIdx;
